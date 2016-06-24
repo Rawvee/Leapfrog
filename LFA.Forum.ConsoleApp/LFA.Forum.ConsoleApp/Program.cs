3b009fa7cac89342269bcf7116dcced997ee5296
rawvee <rawveekrestha@gmail.com>
2016-06-24 08:00:24 +0545

    Three Times Login option

--- LFA.Forum.ConsoleApp/LFA.Forum.ConsoleApp/Program.cs
+++ LFA.Forum.ConsoleApp/LFA.Forum.ConsoleApp/Program.cs
@@ -4,12 +4,53 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace LFA.Forum.ConsoleApp
+namespace loginauthentication
 {
-    class Program
+    internal class Program
     {
-        static void Main(string[] args)
+        private static void Main(string[] args)
         {
+            String username;
+            String password;
+            String[,] accnts = { { "mukes", "dada" }, { "admin", "fafa" }, { "admin", "gaga" } };
+            int row;
+            bool isValideUser = false;
+            for (int x = 3; x >= 1; x--)
+            {
+                Console.WriteLine("You have " + x + " attempt/s.");
+                Console.Write("Enter Username>> ");
+                username = Console.ReadLine();
+                Console.Write("Enter Password>> ");
+                password = Console.ReadLine();
+                for (row = 0; row < 3; row++)
+                {
+                    if (username.Equals(accnts[row, 0]) && password.Equals(accnts[row, 1]))
+                    {
+                        Console.WriteLine("Welcome " + accnts[row, 0] + "!");
+                        isValideUser = true;
+                        break;
+                    }
+                }
+                if (!isValideUser)
+                {
+                    Console.WriteLine("Invalid Input.");
+                    if (x != 1)
+                    {
+                        Console.WriteLine("Please Try Again.");
+                        Console.Write("\n");
+                    }
+                    else if (x.Equals(1))
+                    {
+                        Console.Write("Goodbye!");
+                        break;
+                    }
+                }
+                else
+                {
+                    break;
+                }
+            }
+            Console.ReadKey();
         }
     }
 }
